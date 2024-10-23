e6cc6fca1b51e23594ffb7afa3a8cd23c0944800
YOON DONGJU <meslit@naver.com>
2024-10-23 23:41:18 +0900

    v0.33
    1. 회원가입 업종선택부분 수정
    
    추후 항목
    이력서 작성부분 음성녹음만 체크
    프로필값 왜 못불러오는지 체크

--- site/accounts/models.py
+++ site/accounts/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15, null=True, blank=True)
-    address = models.CharField(max_length=255, null=True, blank=True)
-    detailed_address = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)  # 전화번호 필드
+    address = models.CharField(max_length=255, blank=True, null=True)
+    detailed_address = models.CharField(max_length=255, blank=True, null=True)
+    business_types = models.JSONField(default=list, blank=True)  # 업종을 리스트 형식으로 저장
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
